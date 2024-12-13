8c947078aa42086bd1df9dbd23844360f46928c8
Ron111104 <rc071011ggmu@gmail.com>
2024-12-13 18:41:58 +0530

    commit

--- fplmate_backend/basedir.py
+++ fplmate_backend/basedir.py
@@ -7,5 +7,16 @@ class BaseDirectoryView(View):
     def get(self, request):
         return JsonResponse({
             'BASE_DIR': str(settings.BASE_DIR),
-            'Absolute BASE_DIR': os.path.abspath(str(settings.BASE_DIR))
+            'Absolute BASE_DIR': os.path.abspath(str(settings.BASE_DIR)),
+            'Current Directory': os.getcwd(),
+            'Directory Contents': os.listdir(settings.BASE_DIR),
+            'Full Path Details': {
+                'contents': [
+                    {
+                        'name': item,
+                        'is_dir': os.path.isdir(os.path.join(settings.BASE_DIR, item)),
+                        'is_file': os.path.isfile(os.path.join(settings.BASE_DIR, item))
+                    } for item in os.listdir(settings.BASE_DIR)
+                ]
+            }
         })
